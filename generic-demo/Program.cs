df86b690797f79d0a29d9dcc9c88703921fd2579
pr-su1432 <103477040+pr-su1432@users.noreply.github.com>
2022-05-28 13:01:58 +0530

    Delete Program.cs

--- generic-demo/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-generic_demo.Example<int>   example = new generic_demo.Example<int>();
-Console.WriteLine(example.maximumnumofintegers( 2, 5,9));
-
