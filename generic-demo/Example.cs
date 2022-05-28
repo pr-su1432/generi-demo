9f5a81638b8cdd098a1713c07d42808fe93ae913
[prasanna] <prasanna>
2022-05-28 12:43:52 +0530

    [prasanna]: calculating max value

---
+++ generic-demo/Example.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generic_demo
+{
+    public class Example<T>where T:IComparable
+    {
+        public int Maximumnumofintegers(int firstvalue, int secondvalue, int thirdvale)
+        {
+            if(firstvalue>secondvalue && firstvalue>thirdvale)
+            {
+                return firstvalue;
+                
+            }
+            else if(secondvalue > firstvalue && secondvalue > thirdvale)
+            {
+                return secondvalue;
+         
+            }
+            else
+            {
+                return thirdvale;
+            }
+
+        }
+
+        internal bool maximumnumOfIntegers(int v1, int v2, int v3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ generic-demo/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+generic_demo.Example<int>   example = new generic_demo.Example<int>();
+Console.WriteLine(example.maximumnumofintegers( 2, 5,9));
+
